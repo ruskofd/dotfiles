d1146bb1b4525f8f4b8c8c0e8c0c9505b665de9a
Florian Bobin <fbobin@tuta.io>
2021-07-10 23:46:41 +0200

    Delete powershell directory

--- powershell/profile.ps1
+++
@@ -1,3 +0,0 @@
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
